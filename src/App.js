712c9bea44d981a739b62e497174cce626a9ec60
SURAJGUPTA0414 <sandygupta0414@gmail.com>
2023-01-12 01:08:16 +0530

    textutil video10 commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./Component/Navbar";
-import Form from "./Component/Form";
+// import Form from "./Component/Form";
+import About from "./Component/About";
 
 function App() {
   return (
     <div>
       <Navbar TextUtil="TextUtil" Home="Home" About="About" Contact="Contact" Search="Search" />
-      <Form heading="Enter your text to analyze"/>
+      {/* <Form heading="Enter your text to analyze"/> */}
+      <About />
       </div>
   );
 }
